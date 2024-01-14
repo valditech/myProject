b33370dce321eee298d7eb8200296ce1157c5654
valditech <valdinoel88@gmail.com>
2024-01-13 21:49:25 -0500

    reactJs

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { LandinPage} from "./pages/LandinPage"
+import "./styles/index.css"
+
+
+
+const App = () => {
+  return(
+    <div className="app">
+    <LandinPage/>
+    </div>
+   
+    
+  )
+  
+}
+export default App
